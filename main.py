ef5608b0f026436c3727466362cd141c5c27863f
Bashiho <basilrdv74@gmail.com>
2025-05-12 11:54:15 -0700

    Fixed several issues, only remaining feature before "release" is allowing for !!uhoh > !!stop > !!uhoh to work and start playing again

--- main.py
+++ main.py
@@ -8,17 +8,15 @@ from dotenv import load_dotenv
 from nacl.secret import Aead
 
 """ TBD, order of priority
-    Skip no worky
-    Name command Doesn't work properly
-    Test bot commands
+    When rerunning after stop, doesn't start play
     Don't download already downloaded songs, separate command to update pl
     Doesn't properly check if user is in vc, runs and downloads songs w/o user being in vc
     Create ReadMe
     Doesn't move to diff vc when reusing command
     Test adjusting max_workers to larger numbers for potential performance improvements
 
-Reference: https://github.com/SpaceCowboyZZ/music-bot-yt-dlp/blob/main/main.py
- """
+    Reference: https://github.com/SpaceCowboyZZ/music-bot-yt-dlp/blob/main/main.py
+"""
 #sets bot permissions
 intents = discord.Intents.default() #sets defaults
 #these might be included in the default and thus would be unnecessary, can test running w/o
@@ -29,9 +27,9 @@ intents.guild_messages = True #see messages in servers
 #initiates bot with command prefix as !! and intents as listed above
 bot = commands.Bot(command_prefix='!!', intents=intents)
 #global vars
-queue = [] #queue of songs
-title = None #save title of currently playing song for use in title command
-url = None
+queue = []
+bot.songName = None
+bot.stop = False
 bot.play_status = False #if bot is playing or not
 executor = ThreadPoolExecutor(max_workers=4) #num of concurrent processes, used when downloading songs
 
@@ -40,13 +38,17 @@ test= 'https://www.youtube.com/playlist?list=PLzFA48i-nuXYFLBJ86iFEuAeoH9yS3bRm'
 
 #main method to load bad music pl
 async def doBad(ctx):
-    await ctx.send(f'Making The Bad')
-    #calls playlist method to move info about playlist into data1
-    data1 = await playlist(ctx)
-    data = data1.copy()
-    #lines up songs in queue and calls playNow()
-    queue.extend(data)
-    await playNow(ctx, data, queue) 
+    voice_client, inVC = await checkVC(ctx)
+    if inVC:
+        await ctx.send(f'Making The Bad')
+        bot.stop = False
+        # Calls playlist method to move info about playlist into data1
+        data1 = await playlist(ctx)
+        data = data1.copy()
+        queue.extend(data)
+        await playNow(ctx, data, queue) 
+    else:
+        await ctx.send("Not in vc stinky")
 
 #used for downloading playlist
 async def playlist(ctx):
@@ -78,35 +80,39 @@ async def playNow(ctx, data, queue):
     url = queue.pop(0)
     ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}   
     bot.play_status = True
+    # global title
     #Recursive method for playing songs after prev song ends
     def after(error):
+        # global title
         if error:
             print(error)
                 
-        if queue:
+        if queue and not bot.stop:
             #If songs in queue, lines up next song then plays
             url = queue.pop(0)
-            title = f'{url[0]}'
+            bot.songName = f'{url[0]}'
             ctx.voice_client.play(discord.FFmpegPCMAudio(url[1], **ffmpeg_options), after=lambda e: after(e))
-        else:
+        elif not bot.stop:
             #if nothing in queue, reloads playlist from data and repeats
             queue.extend(data)
             url = queue.pop(0)
-            title = f'{url[0]}'
+            bot.songName = f'{url[0]}'
             ctx.voice_client.play(discord.FFmpegPCMAudio(url[1], **ffmpeg_options), after=lambda e: after(e))
+        else: 
+            return
     #Starts playing of first song in queue
-    title = f'{url[0]}'
+    bot.songName = f'{url[0]}'
     ctx.voice_client.play(discord.FFmpegPCMAudio(url[1], **ffmpeg_options), after=lambda e: after(e))
     
 #moves bot to user's vc
 async def moveVC(ctx):
-    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice_client, inVC = await checkVC(ctx)
     if not voice_client:
-        if ctx.author.voice:
+        if inVC:
             voice_channel = ctx.author.voice.channel
             voice_client = await voice_channel.connect()
             
-        elif not voice_client:
+        else:
             await ctx.send("Not in vc stinky")
             
     elif ctx.author.voice.channel != bot.in_chat:
@@ -115,11 +121,13 @@ async def moveVC(ctx):
 
     return voice_client
 
-async def getTitle(ctx):
-    if title:
-        await ctx.send(title)
+# Checks if user is in VC before doing anything
+async def checkVC(ctx):
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if not voice_client or ctx.author.voice:
+        return voice_client, True
     else:
-        await ctx.send("Nothing is playing")
+        return voice_client, False
 
 #main class of bot
 class Mandy(commands.Cog):
@@ -127,62 +135,70 @@ class Mandy(commands.Cog):
         self.client = client
         self.queue = []
             
-    #sends message containing link to the playlist for user ease of access
-    @commands.command()
-    async def link(self,ctx):
-        await ctx.send('https://www.youtube.com/playlist?list=PLIJH8L_jdxO8ingMAyaOj4cuvZW4Or8l5')
-        
-    #skips current song
-    @commands.command()
-    async def skip(self, ctx):
-        #if in vc and a song is loaded, stops curr song and starts next
-        if ctx.voice_client:
-            #Try replacing w/ .stop() and see if it still causes problems
-            ctx.voice_client.pause()
-            await playNow(ctx, url = queue.pop(0))
-        #if not running and queue is empty, stops curr song and sets play_status to false
-        #Implement a way for it to reload playlist and continue playing
-        elif not queue:
-            ctx.voice_client.stop()
-            bot.play_status = False
-        else:
-            await ctx.send('Not playing currently')
-    
-    #pauses song
-    @commands.command()
-    async def pause(self, ctx):
-        bot.play_status = False
-        await ctx.send('Paused :)')
-        ctx.voice_client.pause()
-        
-    #resumes song
-    @commands.command()
-    async def play(self, ctx):
-        bot.play_status = True
-        ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-        await ctx.send('Resumed')
-        ctx.voice_client.resume()
-       
-    #command to stop play completely
-    @commands.command()
-    async def stop(self, ctx):
-        #if bot is playing, clears queue and curr song info and stops bot
-        if bot.play_status:
-            queue = []
-            ctx.voice_client.stop()
-            bot.play_status = False
-        else:
-            await ctx.send('Not playing anything')
-
     #loads and begins play of playlist of bad music, main function of Mandy       
     @commands.command()
     async def uhoh(self, ctx):
         await doBad(ctx)
 
-    #command to send the title of the currently playing song
+    @commands.command()
+    async def link(self,ctx):
+        await ctx.send('https://www.youtube.com/playlist?list=PLIJH8L_jdxO8ingMAyaOj4cuvZW4Or8l5')
+        
+    @commands.command()
+    async def skip(self, ctx):
+        #if in vc and a song is loaded, stops curr song and starts next
+        if ctx.voice_client:
+            await ctx.send("skipped")
+            ctx.voice_client.stop()
+            # await playNow(ctx, url = queue.pop(0))
+        #if not running and queue is empty, stops curr song and sets play_status to false
+        #Implement a way for it to reload playlist and continue playing
+        else:
+            await ctx.send('Not playing currently')
+    
+    @commands.command()
+    async def pause(self, ctx):
+        if bot.play_status:
+            bot.play_status = False
+            await ctx.send('Paused :)')
+            ctx.voice_client.pause()
+        elif not bot.stop:
+            await ctx.send("Already paused")
+        else:
+            await ctx.send("Bot is already stopped")
+        
+    @commands.command()
+    async def play(self, ctx):
+        if bot.play_status:
+            await ctx.send("Already playing")
+        elif not bot.stop:
+            bot.play_status = True
+            ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+            await ctx.send('Resumed')
+            ctx.voice_client.resume()
+        else:
+            await ctx.send("Bot is stopped, run !!uhoh to start it again")
+       
+    @commands.command()
+    async def stop(self, ctx):
+        voice_client, inVC = await checkVC(ctx)
+        if not voice_client:
+            return
+        #if bot is playing, clears queue and curr song info and stops bot
+        if bot.play_status:
+            queue = []
+            bot.play_status = False
+            bot.stop = True
+            voice_client.stop()
+        elif not bot.stop:
+            await ctx.send('Not playing anything')
+        else:
+            await ctx.send("Bot is already stopped")
+
     @commands.command()
     async def name(self, ctx):
-        await getTitle(ctx)
+        await ctx.send(ctx.bot.songName)
+
         
 async def main():
     #loads token from .env file
